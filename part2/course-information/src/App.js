81fc5bae3fc453ef32aac9c71460b1ea2b479304
Ilkka S <ilkka.sip@gmail.com>
2021-07-25 13:33:43 +0300

    2.2 & 2.3

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -45,10 +45,14 @@ const Content = ({parts}) => {
   return(
     <div>
       {parts.map((part) => <Part key={part.id} part={part}/> )}
+      <Total parts={parts}/>
     </div>
   )
 } 
 const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-const Total = ({parts}) => <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises }</p>
+const Total = ({parts}) => {
+  const exercisesTotal = parts.map((part) => part.exercises).reduce((acc, cur) => acc+cur)
+  return(<p>Number of exercises: {exercisesTotal}</p>)
+}
 
 export default App
