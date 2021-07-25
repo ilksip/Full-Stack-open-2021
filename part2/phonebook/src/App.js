c94bb87a89c700e4c79a62b395a36c770ff501dd
Ilkka S <ilkka.sip@gmail.com>
2021-07-25 19:22:48 +0300

    2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -21,7 +21,11 @@ const App = () => {
     } 
     else {
       const newPerson = {name: newName, number: newNumber}
-      setPersons(persons.concat(newPerson))
+      axios.post("http://localhost:3001/persons", newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
+      
     }
 
     setNewName("")
