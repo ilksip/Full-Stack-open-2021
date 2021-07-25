6242220fa852845a94128bd1db49fd5493e3504a
Ilkka S <ilkka.sip@gmail.com>
2021-07-25 12:37:12 +0300

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,14 +12,18 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-  
-  const handleClick = () =>
-    setSelected(Math.floor(Math.random()*anecdotes.length))
-
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+  const handleVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
   return (
     <div>
+      <button onClick={() => handleVote()}>vote</button>
+      <button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))}>next anecdote</button>
       {anecdotes[selected]}
-      <button onClick={() => handleClick()}>next anecdote</button>
+      <p>has {points[selected]} votes</p>
     </div>
   )
 }
