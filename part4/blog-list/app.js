5371a9699762e9ee694ca6b96673ad00bb67bd31
Ilkka S <ilkka.sip@gmail.com>
2021-07-31 13:23:26 +0300

    added eslint & fixed issues

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,8 +1,8 @@
-const config = require('./utils/config')
-const express = require('express')
+const config = require("./utils/config")
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const cors = require("cors")
+const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
