fa912d5ceb5f81194eccc74b5f1f232227546ac6
Ilkka S <ilkka.sip@gmail.com>
2021-07-31 14:19:51 +0300

    4.4

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,8 +1,23 @@
 const listHelper = require("../utils/list_helper")
-
+const blog_examples = require("../utils/blog_examples")
 test("dummy returns one", () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
+})
+
+describe("total likes are correct", () => {
+    test("when list has only one blog", () => {
+        const likes = listHelper.totalLikes(blog_examples.oneBlog)
+        expect(likes).toBe(7)
+    })
+    test("when list has zero blogs", () => {
+        const likes = listHelper.totalLikes(blog_examples.noBlogs)
+        expect(likes).toBe(0)
+    })
+    test("when there are multiple blogs", () => {
+        const likes = listHelper.totalLikes(blog_examples.all)
+        expect(likes).toBe(36)
+    })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (acc, cur) => acc + cur.likes
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
