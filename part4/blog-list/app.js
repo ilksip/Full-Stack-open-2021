c3bfb09aa5013e326d444d55b12e9b877ba51334
Ilkka S <ilkka.sip@gmail.com>
2021-08-03 13:54:15 +0300

    4.20: made token extraction as middleware

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,13 +7,18 @@ const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
-const errorHandler = require("./utils/errorHandler")
+const middleware = require("./utils/middleware")
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/login", loginRouter)
+app.use("/api/blogs", middleware.tokenExtractor)
 app.use("/api/blogs", blogsRouter)
+
 app.use("/api/users", usersRouter)
-app.use(errorHandler)
+
+
+app.use("/api/login", loginRouter)
+
+app.use(middleware.errorHandler)
 module.exports = app
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = request => {
-    const authorization = request.get("authorization")
-    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-        return authorization.substring(7)
-    }  
-    return null
-}
-
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({}).populate("user", {username: 1, name: 1})
         
@@ -19,9 +11,8 @@ blogsRouter.get("/", async (request, response) => {
   
 blogsRouter.post("/", async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({error: "token missing or invalid"})
     }
     const user = await User.findById(decodedToken.id)
@@ -41,7 +32,6 @@ blogsRouter.post("/", async (request, response) => {
         await user.save()
         response.status(201).json(savedBlog)
     }
-    
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
@@ -59,8 +49,6 @@ blogsRouter.put("/:id", async (request, response) => {
     }
     const update = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
     response.json(update)
-
-    
 })
 
 module.exports = blogsRouter
